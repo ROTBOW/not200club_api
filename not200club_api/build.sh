7395031b0d42a4d4d3b82a39498d42936e2cddeb
Josiah Leon <sjosiahleon@gmail.com>
2023-08-01 14:38:31 -0700

    setup for hosting

---
+++ not200club_api/build.sh
@@ -0,0 +1,4 @@
+cd ..
+pip install -r requirements.txt
+cd not200club_api
+python manage.py migrate
--- not200club_api/not200club_api/settings.py
+++ not200club_api/not200club_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,14 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mphfd^jk8lhq)3(iy8-yolm3dbwn=4b6+*nmbof(lebkx+rtnb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('PRODUCTION', None) != 'True'
 
 ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-    "http://127.0.0.1:8000",
-    "http://localhost:3000",
+    "*",
 ]
 
 
@@ -79,13 +78,16 @@ WSGI_APPLICATION = 'not200club_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('PRODUCTION') != 'True':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config(conn_max_age=60)}
 
 
 # Password validation
